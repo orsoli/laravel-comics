1d1366a23dab8198f00b14953955fc4186163301
orsoli <fljcoli@gmail.com>
2024-10-24 01:29:40 +0200

    Added modification

--- resources/views/partials/main-header.blade.php
+++ resources/views/partials/main-header.blade.php
@@ -5,7 +5,7 @@
             <div class="card border border-0">
                 <img src="{{$movies[0]["imgUrl"]}}" class="card-img-top" alt="{{$movies[0]["title"]}} image">
                 <div class="card-body">
-                    <a href="" class="text-dark text-decoration-none"><h5 class="card-title fs-1">{{$movies[0]["title"]}}</h5></a>
+                    <a href="{{route("show", $movies[0]["id"])}}" class="text-dark text-decoration-none"><h5 class="card-title fs-1">{{$movies[0]["title"]}}</h5></a>
                     <p class="card-text">{{$movies[0]["type"]}}</p>
                 </div>
             </div>
@@ -14,14 +14,14 @@
             <div class="card">
                 <img src="{{$movies[1]["imgUrl"]}}" class="card-img-top" alt="img">
                 <div class="card-body">
-                    <a href="#"><h5 class="card-title">{{$movies[1]["title"]}}</h5></a>
+                    <a href="{{route("show", $movies[1]["id"])}}"><h5 class="card-title">{{$movies[1]["title"]}}</h5></a>
                     <p class="card-text">{{$movies[1]["type"]}}</p>
                 </div>
             </div>
             <div class="card">
                 <img src="{{$movies[2]["imgUrl"]}}" class="card-img-top" alt="img">
                 <div class="card-body">
-                    <a href="#"><h5 class="card-title">{{$movies[2]["title"]}}</h5></a>
+                    <a href="{{route("show", $movies[2]["id"])}}"><h5 class="card-title">{{$movies[2]["title"]}}</h5></a>
                     <p class="card-text">{{$movies[2]["type"]}}</p>
                 </div>
             </div>
